ec3aaa4bd75f8070da6db6f189c86c558733cfbb
flaneuse <flaneuse@users.noreply.github.com>
2022-11-21 09:16:53 -0600

    add relative routing

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/hughes-travels/'
+  : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: bootstrapSassAbstractsImports.join('\n')
+      },
+      scss: {
+        additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
+      }
+    }
+  }
+}
